ec5a5bb65f98c761eab9affc4cd3cc468957bb7a
Michael Mücke <michael.muecke@auronet.de>
2022-07-07 14:22:19 +0200

    fixed negative decimals

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 "use strict";
 
 const CURRENCY_POSITION = {
-    LEFT: 'LEFT',
-    RIGHT: 'RIGHT'
+  LEFT: "LEFT",
+  RIGHT: "RIGHT",
 };
 
 const SHOW_DECIMALS = {
-    ALWAYS: 'ALWAYS',
-    NEVER: 'NEVER',
-    IF_NEEDED: 'IF_NEEDED',
-    AS_IS: 'AS_IS'
+  ALWAYS: "ALWAYS",
+  NEVER: "NEVER",
+  IF_NEEDED: "IF_NEEDED",
+  AS_IS: "AS_IS",
 };
 
 const DEFAULT_OPTIONS = {
-    currency: '',
-    showDecimals: SHOW_DECIMALS.ALWAYS,
-    thousandSeparator: ',',
-    decimalSeparator: '.',
-    currencyPosition: CURRENCY_POSITION.RIGHT,
-    decimalsDigits: 2,
-    spacing: true,
-    arithmeticalRounding: false
+  currency: "",
+  showDecimals: SHOW_DECIMALS.ALWAYS,
+  thousandSeparator: ",",
+  decimalSeparator: ".",
+  currencyPosition: CURRENCY_POSITION.RIGHT,
+  decimalsDigits: 2,
+  spacing: true,
+  arithmeticalRounding: false,
 };
 /**
  * Returns value or default value if the value is not defined
@@ -29,22 +29,37 @@ const DEFAULT_OPTIONS = {
  * @param {*} defaultValue
  */
 function parse(value, defaultValue) {
-    return value !== undefined ? value : defaultValue;
-};
+  return value !== undefined ? value : defaultValue;
+}
 
 function parseOptions(options) {
-    if (!options) { return DEFAULT_OPTIONS };
-    return {
-        currency: options.currency || DEFAULT_OPTIONS.currency,
-        thousandSeparator: parse(options.thousandSeparator, DEFAULT_OPTIONS.thousandSeparator),
-        decimalSeparator: parse(options.decimalSeparator, DEFAULT_OPTIONS.decimalSeparator),
-        showDecimals: options.showDecimals || DEFAULT_OPTIONS.showDecimals,
-        decimalsDigits: parse(options.decimalsDigits, DEFAULT_OPTIONS.decimalsDigits),
-        currencyPosition: options.currencyPosition || DEFAULT_OPTIONS.currencyPosition,
-        spacing: parse(options.spacing, DEFAULT_OPTIONS.spacing),
-        arithmeticalRounding: parse(options.arithmeticalRounding, DEFAULT_OPTIONS.arithmeticalRounding)
-    };
-};
+  if (!options) {
+    return DEFAULT_OPTIONS;
+  }
+  return {
+    currency: options.currency || DEFAULT_OPTIONS.currency,
+    thousandSeparator: parse(
+      options.thousandSeparator,
+      DEFAULT_OPTIONS.thousandSeparator
+    ),
+    decimalSeparator: parse(
+      options.decimalSeparator,
+      DEFAULT_OPTIONS.decimalSeparator
+    ),
+    showDecimals: options.showDecimals || DEFAULT_OPTIONS.showDecimals,
+    decimalsDigits: parse(
+      options.decimalsDigits,
+      DEFAULT_OPTIONS.decimalsDigits
+    ),
+    currencyPosition:
+      options.currencyPosition || DEFAULT_OPTIONS.currencyPosition,
+    spacing: parse(options.spacing, DEFAULT_OPTIONS.spacing),
+    arithmeticalRounding: parse(
+      options.arithmeticalRounding,
+      DEFAULT_OPTIONS.arithmeticalRounding
+    ),
+  };
+}
 /**
  * Inserts {separator} at every thousand position of number text, like:
  * For separator = ",": 1234567 -> 1,234,567
@@ -52,14 +67,21 @@ function parseOptions(options) {
  * @param {string} separator - separator character
  */
 function withThousandSeparator(numberText, separator) {
-    return numberText.split('').reduce(function(returnText, currentLetter, index) {
-        const maybeSeparator = (index < numberText.length - 1 && (numberText.length - index - 1) % 3 === 0 && currentLetter!== '-') ? separator : ''
-        return returnText + currentLetter + maybeSeparator;
-    }, '');
-};
+  return numberText
+    .split("")
+    .reduce(function (returnText, currentLetter, index) {
+      const maybeSeparator =
+        index < numberText.length - 1 &&
+        (numberText.length - index - 1) % 3 === 0 &&
+        currentLetter !== "-"
+          ? separator
+          : "";
+      return returnText + currentLetter + maybeSeparator;
+    }, "");
+}
 
 function roundToPlace(number, place) {
-    return +(Math.round(number + "e+" + place)  + "e-" + place);
+  return +(Math.round(number + "e+" + place) + "e-" + place);
 }
 /**
  *
@@ -75,111 +97,161 @@ function roundToPlace(number, place) {
  * @param {boolean} [options.arithmeticalRounding=false] - Enables regular rounding without tie-breaking. By default: false
  */
 function format(number, options) {
-    const opts = parseOptions(options);
+  const opts = parseOptions(options);
 
-    const fullPriceInNormalFormat = (opts.showDecimals === SHOW_DECIMALS.AS_IS) ? number.toString() :
-      parseFloat((opts.arithmeticalRounding ? roundToPlace(number, opts.decimalsDigits) : +number)
-        .toString())
-        .toFixed(opts.showDecimals === SHOW_DECIMALS.NEVER ? 0 : opts.decimalsDigits);
+  const fullPriceInNormalFormat =
+    opts.showDecimals === SHOW_DECIMALS.AS_IS
+      ? number.toString()
+      : parseFloat(
+          (opts.arithmeticalRounding
+            ? roundToPlace(number, opts.decimalsDigits)
+            : +number
+          ).toString()
+        ).toFixed(
+          opts.showDecimals === SHOW_DECIMALS.NEVER ? 0 : opts.decimalsDigits
+        );
 
-    if (isNaN(fullPriceInNormalFormat)) return number;
+  if (isNaN(fullPriceInNormalFormat)) return number;
 
-    const priceAndDecimals = fullPriceInNormalFormat.split('.');
-    const integerPricePart = priceAndDecimals[0];
-    const decimalPricePart = priceAndDecimals[1] || '';
+  const priceAndDecimals = fullPriceInNormalFormat.split(".");
+  const integerPricePart = priceAndDecimals[0];
+  const decimalPricePart = priceAndDecimals[1] || "";
 
-    const decimals = (
-      (opts.showDecimals === SHOW_DECIMALS.ALWAYS) ||
-      ( (opts.showDecimals === SHOW_DECIMALS.IF_NEEDED && +decimalPricePart > 0 ) ||
-        (opts.showDecimals === SHOW_DECIMALS.AS_IS && decimalPricePart.length > 0)
-    ) ) ? opts.decimalSeparator + decimalPricePart : '';
+  const decimals =
+    opts.showDecimals === SHOW_DECIMALS.ALWAYS ||
+    (opts.showDecimals === SHOW_DECIMALS.IF_NEEDED && +decimalPricePart > 0) ||
+    (opts.showDecimals === SHOW_DECIMALS.AS_IS && decimalPricePart.length > 0)
+      ? opts.decimalSeparator + decimalPricePart
+      : "";
 
-    const price = withThousandSeparator(integerPricePart, opts.thousandSeparator) + decimals;
+  const price =
+    withThousandSeparator(integerPricePart, opts.thousandSeparator) + decimals;
 
-    if (opts.currency) {
-        const priceCurrencySpacing = opts.spacing ? ' ' : '';
-        return opts.currencyPosition === CURRENCY_POSITION.LEFT ?
-            opts.currency +  priceCurrencySpacing + price :
-            price + priceCurrencySpacing + opts.currency
-    } else {
-        return price;
-    }
-};
+  if (opts.currency) {
+    const priceCurrencySpacing = opts.spacing ? " " : "";
+    return opts.currencyPosition === CURRENCY_POSITION.LEFT
+      ? opts.currency + priceCurrencySpacing + price
+      : price + priceCurrencySpacing + opts.currency;
+  } else {
+    return price;
+  }
+}
 
 function isProbablyDecimalSeparator(text) {
-    // I assume the only feasible decimal separators could be . and ,
-    return /^[\.,]$/.test(text);
-};
+  // I assume the only feasible decimal separators could be . and ,
+  return /^[\.,]$/.test(text);
+}
 
 function unformat(text, options) {
-    const opts = parseOptions(options);
-    const customDecimalSeparator = options && options.decimalSeparator;
+  const opts = parseOptions(options);
+  const customDecimalSeparator = options && options.decimalSeparator;
 
-    const withoutWhiteSpace = text.replace(/\s/g,'');
+  const withoutWhiteSpace = text.replace(/\s/g, "");
 
-    // split it into (letters, notLetter, letters...) groups
-    const lettersNotLettersGroups = withoutWhiteSpace.match(/[0-9]+|[^0-9]+/gi);
-    const groups = lettersNotLettersGroups.map(function(group, i) {
-        return {
-            value: group,
-            isNumber: (/[0-9]+/).test(group),
-            index: i
-        }
+  // split it into (letters, notLetter, letters...) groups
+  const lettersNotLettersGroups = withoutWhiteSpace.match(/[0-9]+|[^0-9]+/gi);
+  const groups = lettersNotLettersGroups.map(function (group, i) {
+    return {
+      value: group,
+      isNumber: /[0-9]+/.test(group),
+      index: i,
+    };
+  });
+
+  const numbers = groups.filter(function (group) {
+    return group.isNumber;
+  });
+  const separators = groups.filter(function (group) {
+    return !group.isNumber;
+  });
+
+  const firstNumberGroup = numbers[0];
+  const lastNumberGroup = numbers[numbers.length - 1];
+
+  const firstNonZeroNumberGroup =
+    firstNumberGroup.value > 0
+      ? firstNumberGroup
+      : numbers.length > 1
+      ? numbers[1].value > 0
+        ? numbers[1]
+        : undefined
+      : undefined;
+
+  if (!firstNonZeroNumberGroup) {
+    return 0;
+  }
+
+  // Decimals presence test 1: .123 -> first separator lays before the first number group
+  const separatorBeforeFirstNumber =
+    firstNonZeroNumberGroup.index > 0
+      ? groups[firstNonZeroNumberGroup.index - 1]
+      : undefined;
+  const firstSeparatorIsDecimal =
+    separatorBeforeFirstNumber &&
+    separators.length === 1 &&
+    isProbablyDecimalSeparator(separatorBeforeFirstNumber.value);
+
+  // Decimals presence test 2: 123.456,789 -> last separator is different to the rest of separators
+  const separatorBeforeLastNumberIndex = lastNumberGroup.index - 1;
+  const separatorBeforeLastNumber =
+    separatorBeforeLastNumberIndex >= 0 &&
+    groups[separatorBeforeLastNumberIndex];
+  const separatorsPastFirstNumberButLastNumberSeparator = separators
+    .filter(function (group) {
+      return (
+        group.index > firstNumberGroup.index &&
+        group.index < separatorBeforeLastNumberIndex
+      );
+    })
+    .map(function (separator) {
+      return separator.value;
     });
 
-    const numbers = groups.filter(function(group) { return group.isNumber; });
-    const separators = groups.filter(function(group) { return !group.isNumber; });
+  const lastSeparatorBetweenTheNumberIsDifferentThanOthers =
+    separatorsPastFirstNumberButLastNumberSeparator.length > 0 &&
+    separatorsPastFirstNumberButLastNumberSeparator.indexOf(
+      separatorBeforeLastNumber.value
+    ) < 0;
 
-    const firstNumberGroup = numbers[0];
-    const lastNumberGroup = numbers[numbers.length - 1];
+  // Decimals presence test 3: user specified explicitly a decimal separator and it is a last separator between the numbers
+  const lastSeparatorIsAsSpecifiedByUser =
+    separatorBeforeLastNumber &&
+    separatorBeforeLastNumber.value === customDecimalSeparator;
 
-    const firstNonZeroNumberGroup = firstNumberGroup.value > 0 ? firstNumberGroup : numbers.length > 1 ? numbers[1].value > 0 ? numbers[1] : undefined: undefined;
-    
-    if (!firstNonZeroNumberGroup) {
-        return 0;
-    }
+  // Decimals presence test 4: last group matches the decimals length (default: 2)
+  // And there's a character before the last group that looks like a decimal separator
+  const lastGroupMatchesDecimalsLength =
+    lastNumberGroup.value.length === opts.decimalsDigits &&
+    separatorBeforeLastNumber &&
+    isProbablyDecimalSeparator(separatorBeforeLastNumber.value);
 
-    // Decimals presence test 1: .123 -> first separator lays before the first number group
-    const separatorBeforeFirstNumber = firstNonZeroNumberGroup.index > 0 ? groups[firstNonZeroNumberGroup.index - 1] : undefined;
-    const firstSeparatorIsDecimal = separatorBeforeFirstNumber && separators.length === 1 && isProbablyDecimalSeparator(separatorBeforeFirstNumber.value);
+  // Combine all decimal presence tests
+  const lastGroupIsProbablyDecimal = customDecimalSeparator
+    ? lastSeparatorIsAsSpecifiedByUser
+    : lastGroupMatchesDecimalsLength ||
+      lastSeparatorIsAsSpecifiedByUser ||
+      firstSeparatorIsDecimal ||
+      lastSeparatorBetweenTheNumberIsDifferentThanOthers;
 
-    // Decimals presence test 2: 123.456,789 -> last separator is different to the rest of separators
-    const separatorBeforeLastNumberIndex = lastNumberGroup.index - 1;
-    const separatorBeforeLastNumber = separatorBeforeLastNumberIndex >= 0 && groups[separatorBeforeLastNumberIndex];
-    const separatorsPastFirstNumberButLastNumberSeparator = separators.filter(function(group) {
-        return group.index > firstNumberGroup.index && group.index < separatorBeforeLastNumberIndex;
-    }).map(function(separator) {
-        return separator.value
-    });
+  const probablyDecimalsDigits = lastGroupIsProbablyDecimal
+    ? lastNumberGroup.value.length
+    : 0;
 
-    const lastSeparatorBetweenTheNumberIsDifferentThanOthers = separatorsPastFirstNumberButLastNumberSeparator.length > 0 && 
-    separatorsPastFirstNumberButLastNumberSeparator.indexOf(separatorBeforeLastNumber.value) < 0;
+  const isNegative = separators?.length && separators[0].value === "-";
 
-    // Decimals presence test 3: user specified explicitly a decimal separator and it is a last separator between the numbers
-    const lastSeparatorIsAsSpecifiedByUser = separatorBeforeLastNumber && separatorBeforeLastNumber.value === customDecimalSeparator;
+  const joinedNumber = numbers
+    .map(function (number) {
+      return number.value;
+    })
+    .join("");
 
-    // Decimals presence test 4: last group matches the decimals length (default: 2)
-    // And there's a character before the last group that looks like a decimal separator
-    const lastGroupMatchesDecimalsLength = lastNumberGroup.value.length === opts.decimalsDigits && separatorBeforeLastNumber && isProbablyDecimalSeparator(separatorBeforeLastNumber.value);
-
-    // Combine all decimal presence tests
-    const lastGroupIsProbablyDecimal = customDecimalSeparator ? lastSeparatorIsAsSpecifiedByUser :
-        (lastGroupMatchesDecimalsLength ||
-        lastSeparatorIsAsSpecifiedByUser ||
-        firstSeparatorIsDecimal ||
-        lastSeparatorBetweenTheNumberIsDifferentThanOthers);
-
-    const probablyDecimalsDigits = lastGroupIsProbablyDecimal ? lastNumberGroup.value.length : 0;
-
-    const lastSeparatorText = separatorBeforeFirstNumber && separatorBeforeFirstNumber.value;
-    const isNegative = lastSeparatorText && lastSeparatorText[lastSeparatorText.length - 1] === '-';
-
-    const joinedNumber = numbers.map(function(number) {return number.value }).join('');
-
-    return parseFloat(joinedNumber / Math.pow(10, probablyDecimalsDigits)) * (isNegative ? -1 : 1);
-};
+  return (
+    parseFloat(joinedNumber / Math.pow(10, probablyDecimalsDigits)) *
+    (isNegative ? -1 : 1)
+  );
+}
 
 module.exports = {
-    format,
-    unformat
+  format,
+  unformat,
 };
